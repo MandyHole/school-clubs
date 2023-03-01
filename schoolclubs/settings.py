ca29c13bfd4637bcfbaa4a8bbd1dd80d7788b308
MandyHole <mandyhole17@gmail.com>
2023-03-01 12:31:03 +0000

    Updated Allowed Hosts in settings file

--- schoolclubs/settings.py
+++ schoolclubs/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["school-clubs.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["schoolclubs.herokuapp.com", "localhost"]
 
 
 # Application definition
