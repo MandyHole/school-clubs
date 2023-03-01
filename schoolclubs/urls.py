d63a360e5d67eee067d8508994233017f6069825
MandyHole <mandyhole17@gmail.com>
2023-03-01 22:31:36 +0000

    Set index to view and start nav/hero styling

--- schoolclubs/urls.py
+++ schoolclubs/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pupils.views import get_homepage
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_homepage, name = 'get_homepage')
 ]
+urlpatterns += staticfiles_urlpatterns()
