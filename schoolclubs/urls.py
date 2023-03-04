bcdfdd83656c961cebd1fd32b8d32343687b9842
MandyHole <mandyhole17@gmail.com>
2023-03-04 18:20:05 +0000

    Add user login / add if user statements to index.html

--- schoolclubs/urls.py
+++ schoolclubs/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pupils.views import get_homepage
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_homepage, name = 'get_homepage')
+    path('', get_homepage, name = 'get_homepage'),
+    path('accounts/', include('allauth.urls'))
 ]
 urlpatterns += staticfiles_urlpatterns()
