aa1bec0876d2c9935ec3eebd0b8a336d92f91dda
MandyHole <mandyhole17@gmail.com>
2023-03-04 17:19:23 +0000

    Tweak models

--- pupils/models.py
+++ pupils/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -16,29 +17,29 @@ class Pupil(models.Model):
     first_name_of_pupil = models.CharField(
         max_length=25, null=False, blank=False)
     surname_of_pupil = models.CharField(max_length=25, null=False, blank=False)
-    parent_name = models.CharField(max_length=100)
+    # parent_name = models.ForeignKey(User, on_delete=models.CASCADE)
     parent_email = models.EmailField(max_length=254)
     year_group = models.CharField(max_length=1, choices=YEAR_GROUPS)
 
     def __str__(self):
-        return f'${self.first_name_of_pupil} ${self.surname_of_pupil}'
+        return f'{self.first_name_of_pupil} {self.surname_of_pupil}'
 
 
 class Breakfast(models.Model):
     pupil_name = models.ForeignKey(
         Pupil, on_delete=models.CASCADE, unique_for_date='breakfast_date')
     breakfast_date = models.DateField()
-    confirmation = models.BooleanField()
+    # confirmation = models.BooleanField()
 
     def __str__(self):
-        return self.breakfast_date
+        return f'{self.pupil_name}: {self.breakfast_date}'
 
 
 class Supper(models.Model):
     pupil_name = models.ForeignKey(
         Pupil, on_delete=models.CASCADE, unique_for_date='supper_date')
     supper_date = models.DateField()
-    confirmation = models.BooleanField()
+    # confirmation = models.BooleanField()
 
     def __str__(self):
-        return self.supper_date
+        return f'{self.pupil_name}: {self.supper_date}'
