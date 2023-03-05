d08a1912c9b9da1cf47ce18b20414abc11089b0b
MandyHole <mandyhole17@gmail.com>
2023-03-05 23:30:03 +0000

    Update models to include date ranges

--- pupils/admin.py
+++ pupils/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Pupil, Breakfast, Supper
+from .models import Pupil, Breakfast, Supper, BookClub, Parent
 
 # Register your models here.
 admin.site.register(Pupil)
 admin.site.register(Breakfast)
 admin.site.register(Supper)
+admin.site.register(Parent)
+admin.site.register(BookClub)
